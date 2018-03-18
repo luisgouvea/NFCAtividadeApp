668641d803aad54ac4a6b223ce3af4686761dcb5
Eduardo Silva <luiseduardo.gouvea@hotmail.com>
2018-03-18 00:41:08 -0300

    ajuste para consumir a API

--- app/src/main/java/com/fiquedeolho/nfcatividadeapp/util/ConstantsURIAPI.java
+++ app/src/main/java/com/fiquedeolho/nfcatividadeapp/util/ConstantsURIAPI.java
@@ -2,7 +2,7 @@ package com.fiquedeolho.nfcatividadeapp.util;
 
 public final class ConstantsURIAPI {
 
-//    public static final String AUTENTICACAO = "http://192.168.0.4:57016/api/TesteComunicacao/Comunicar";
+    /*public static final String AUTENTICACAO = "http://192.168.0.10:57016/api/TesteComunicacao/Comunicar";*/
 //    public static final String GETATIVIDADESEXECUTAR = "http://192.168.0.4:57016/api/TesteComunicacao/getAtivExecutar";
 //    public static final String GETDETAILSATIVIDADE = "http://192.168.0.4:57016/api/TesteComunicacao/getDetailsAtividade";
 
