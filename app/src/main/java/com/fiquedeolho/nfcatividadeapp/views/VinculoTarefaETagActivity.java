cf6641a377d4c755873de6dee07520a8366e6392
Eduardo Silva <luiseduardo.gouvea@hotmail.com>
2018-04-08 16:15:14 -0300

    vinculo tag e tarefa

---
+++ app/src/main/java/com/fiquedeolho/nfcatividadeapp/views/VinculoTarefaETagActivity.java
@@ -0,0 +1,15 @@
+package com.fiquedeolho.nfcatividadeapp.views;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.fiquedeolho.nfcatividadeapp.R;
+
+public class VinculoTarefaETagActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vinculo_tarefa_e_tag);
+    }
+}
