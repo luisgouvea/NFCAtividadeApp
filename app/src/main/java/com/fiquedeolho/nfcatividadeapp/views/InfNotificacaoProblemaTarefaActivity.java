54d88a4f6bfb4bc33979f5025b952beb6aa6b674
Eduardo Silva <luiseduardo.gouvea@hotmail.com>
2018-05-12 01:42:49 -0300

    ajustes de notificacoes problema novamente

--- app/src/main/java/com/fiquedeolho/nfcatividadeapp/views/InfNotificacaoProblemaTarefaActivity.java
+++ app/src/main/java/com/fiquedeolho/nfcatividadeapp/views/InfNotificacaoProblemaTarefaActivity.java
@@ -7,14 +7,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.fiquedeolho.nfcatividadeapp.R;
+import com.fiquedeolho.nfcatividadeapp.models.APIError;
+import com.fiquedeolho.nfcatividadeapp.models.NotificacaoUsuario;
 import com.fiquedeolho.nfcatividadeapp.models.NotificacaoUsuarioProblemaTarefa;
+import com.fiquedeolho.nfcatividadeapp.retrofit.implementation.NotificacaoUsuarioImplementation;
 
-public class InfNotificacaoProblemaTarefaActivity extends AppCompatActivity {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class InfNotificacaoProblemaTarefaActivity<T> extends AppCompatActivity implements Callback<T> {
 
     private ViewHolderInfNotificacaoProblemaTarefa mViewHolderInfNotificacaoProblemaTarefa = new ViewHolderInfNotificacaoProblemaTarefa();
     public static NotificacaoUsuarioProblemaTarefa notificacaoUsuarioProblemaTarefa;
+    private Callback<T> requestRetrofit = this;
+    private NotificacaoUsuarioImplementation notificacaoUsuarioImplementation = new NotificacaoUsuarioImplementation();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +57,58 @@ public class InfNotificacaoProblemaTarefaActivity extends AppCompatActivity {
         this.mViewHolderInfNotificacaoProblemaTarefa.mViewCheckBoxCheckRealizado.setEnabled(false);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(notificacaoUsuarioProblemaTarefa.getVisualizada() == false) {
+            updateNotificacaoParaVisualizada();
+        }
+    }
 
+    private void updateNotificacaoParaVisualizada() {
+        NotificacaoUsuario notiUsu = new NotificacaoUsuario();
+        notiUsu.setIdUsuarioNotificado(notificacaoUsuarioProblemaTarefa.getIdUsuarioNotificado());
+        notiUsu.setVisualizada(true);
+        notiUsu.setDescricaoNotificacao(notificacaoUsuarioProblemaTarefa.getDescricaoNotificacao());
+        notiUsu.setIdNotificacaoUsuario(notificacaoUsuarioProblemaTarefa.getIdNotificacaoUsuario());
+        notiUsu.setDataNotificacao(notificacaoUsuarioProblemaTarefa.getDataNotificacao());
+        notificacaoUsuarioImplementation.requestUpdateObject(requestRetrofit, notiUsu);
+    }
+
+
+
+    @Override
+    public void onResponse(Call<T> call, Response<T> response) {
+        requestNotificacaoUsuario(call, response);
+    }
+
+    @Override
+    public void onFailure(Call<T> call, Throwable t) {
+        Toast.makeText(getApplicationContext(), "Ocorreu um erro genérico" + t.getMessage(), Toast.LENGTH_LONG).show();
+    }
+
+
+
+    private void requestNotificacaoUsuario(Call<T> call, Response<T> response) {
+        APIError error = null;
+        Boolean realizado = false;
+        String typeResponse = notificacaoUsuarioImplementation.findResponse(call, response);
+        if (typeResponse != "") {
+            switch (typeResponse) {
+                case "erro":
+                    error = notificacaoUsuarioImplementation.resultError();
+                    if (error.message() != null) {
+                        Toast.makeText(getApplicationContext(), error.message(), Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Ocorreu um erro genérico", Toast.LENGTH_LONG).show();
+                    }
+                    break;
+                case "updateNotificacao":
+                    realizado = notificacaoUsuarioImplementation.resultUpdateObject();
+                    break;
+            }
+        }
+    }
 
     /**
      * Click no botao voltar da activity
